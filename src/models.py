a1bd3bb57a886494dcac5f1e339c956c2de14a6b
Mateus Assis <mateus.d.assis.silva@gmail.com>
2023-12-03 12:04:06 -0300

    feat: add layers' filters number as param

--- src/models.py
+++ src/models.py
@@ -4,13 +4,14 @@ from tensorflow.keras.models import Model
 class ConvAutoEncoder(Model):
   def __init__(self,
                input_shape,
+               conv_filters = (16,32),
                latent_dim = 2):
     super(ConvAutoEncoder, self).__init__()
   
     # Encoder
     encoder_input = layers.Input(shape=input_shape)
-    encoder_layer_1 = layers.Conv2D(16, (3, 3), activation='relu', padding='same', strides=2)(encoder_input)
-    encoder_layer_2 = layers.Conv2D(32, (3, 3), activation='relu', padding='same', strides=2)(encoder_layer_1)
+    encoder_layer_1 = layers.Conv2D(conv_filters[0], (3, 3), activation='relu', padding='same', strides=2)(encoder_input)
+    encoder_layer_2 = layers.Conv2D(conv_filters[1], (3, 3), activation='relu', padding='same', strides=2)(encoder_layer_1)
     encoder_layer_3 = layers.Flatten()(encoder_layer_2)
     encoder_layer_4 = layers.Dense(latent_dim,
                                    kernel_regularizer='l2')(encoder_layer_3)
@@ -20,11 +21,11 @@ class ConvAutoEncoder(Model):
 
     # Decoder 
     decoder_input = layers.Input(shape = (latent_dim,))
-    decoder_layer_1 = layers.Dense(units=32*1689*32,
+    decoder_layer_1 = layers.Dense(units=32*1689*conv_filters[1],
                                    kernel_regularizer='l2')(decoder_input)
-    decoder_layer_2 = layers.Reshape(target_shape=(32,1689,32))(decoder_layer_1)
-    decoder_layer_3 = layers.Conv2DTranspose(32, kernel_size=3, strides=2, activation='relu', padding='same')(decoder_layer_2)
-    decoder_layer_4 = layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same')(decoder_layer_3)
+    decoder_layer_2 = layers.Reshape(target_shape=(32,1689,conv_filters[1]))(decoder_layer_1)
+    decoder_layer_3 = layers.Conv2DTranspose(conv_filters[1], kernel_size=3, strides=2, activation='relu', padding='same')(decoder_layer_2)
+    decoder_layer_4 = layers.Conv2DTranspose(conv_filters[0], kernel_size=3, strides=2, activation='relu', padding='same')(decoder_layer_3)
     decoder_layer_5 = layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')(decoder_layer_4)
     self.decoder = Model(decoder_input, 
                          decoder_layer_5, 
